d4da42d258abe08e9668c6fa4d7aaa741cda54b4
vovkaWeb <vova77557@gmail.com>
2023-01-01 16:39:18 +0200

    /

--- src/index.js
+++ src/index.js
@@ -4,14 +4,57 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+		 counter: this.props.counter
+	  }
+	}
+	
+	incCounter = () => {
+	  if (this.state.counter < 50) {
+		 this.setState(state => ({
+			counter: state.counter + 1
+		 }))
+	  }
+	}
+	
+	decCounter = () => {
+	  if (this.state.counter > -50) {
+		 this.setState(state => ({
+			counter: state.counter - 1
+		 }))
+	  }
+	}
+	
+	rndCounter = () => {
+	  this.setState({
+		 counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
+	  })
+	}
+	
+	resetCounter = () => {
+	  this.setState({
+		 counter: this.props.counter
+	  })
+	}
+	
+	render() {
+	  const {counter} = this.state;
+	  
+	  return (
+		 <div className="app">
+			<div className="counter">{counter}</div>
+			<div className="controls">
+			  <button onClick={this.incCounter}>INC</button>
+			  <button onClick={this.decCounter}>DEC</button>
+			  <button onClick={this.rndCounter}>RND</button>
+			  <button onClick={this.resetCounter}>RESET</button>
+			</div>
+		 </div>
+	  )
+	}
+ }
+ 
+ ReactDOM.render(<App counter={0}/>, document.getElementById('app'));
